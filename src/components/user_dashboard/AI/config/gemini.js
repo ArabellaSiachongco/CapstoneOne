2aa4c5e88d45bb79881e04898fc5422814c71fd4
ArabellaSiachongco <aps9434@students.uc-bcf.edu.ph>
2025-03-11 00:42:48 +0800

    done fixing the env

--- src/components/user_dashboard/AI/config/gemini.js
+++ src/components/user_dashboard/AI/config/gemini.js
@@ -5,7 +5,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.5-pro"
-const API_KEY = "AIzaSyDR3vXyfe-NWbMwQvaypVyOWJKZP15FghI";
+const API_KEY = import.meta.env.VITE_AI_API_KEY;
 
 async function run(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
